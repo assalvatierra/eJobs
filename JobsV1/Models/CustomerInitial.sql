065ef71518f9f6055a28ea61d6d17d2225e12887
Abel S. Salvatierra <assalvatierra@gmail.com>
2018-07-23 15:57:52 +0800

    job.AddPo

--- JobsV1/Models/CustomerInitial.sql
+++ JobsV1/Models/CustomerInitial.sql
@@ -7,12 +7,12 @@ insert into CustCats(CustomerId, CustCategoryId)
 values(3,2),(3,1);
 
 insert into CustEntMains(Name, Address, Contact1, Contact2, iconPath) 
-values('NewCompany.Inc','Davao City','09950753794','09950753794','http://localhost:50382/Images/Customers/Company/organization-40.png');
+values('NewCompany.Inc','Davao City','09950753794','09950753794','/Images/Customers/Company/organization-40.png');
 
 insert into CustEntities(CustEntMainId, CustomerId) 
 values(2,3);
 
-update CustCategories set iconPath = 'http://localhost:50382/Images/Customers/Category/star-filled-40.png' where Id = 1; 
-update CustCategories set iconPath = 'http://localhost:50382/Images/Customers/Category/Active-30.png' where Id = 2; 
-update CustCategories set iconPath = 'http://localhost:50382/Images/Customers/Category//suspended-64.png' where Id = 3; 
-update CustCategories set iconPath = 'http://localhost:50382/Images/Customers/Category/cancel-40.png' where Id = 4; 
+update CustCategories set iconPath = '/Images/Customers/Category/star-filled-40.png' where Id = 1; 
+update CustCategories set iconPath = '/Images/Customers/Category/Active-30.png' where Id = 2; 
+update CustCategories set iconPath = '/Images/Customers/Category//suspended-64.png' where Id = 3; 
+update CustCategories set iconPath = '/Images/Customers/Category/cancel-40.png' where Id = 4; 
