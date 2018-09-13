55669f50a209fae2d18dc2a2817ba45368195697
jahdiel-officepc <jahdielsvillosa@gmail.com>
2018-09-13 15:04:30 +0800

    carrental initial

--- JobsV1/Models/CarRentalInitial.sql
+++ JobsV1/Models/CarRentalInitial.sql
@@ -1,6 +1,4 @@
 ﻿
-
-
 insert into CarCategories (Description, Remarks)
 values ('Rental','');
 
@@ -13,18 +11,25 @@ values ('Van (10 seater)','',1),
 	   ('Sedan','Van',1);
 
 insert into CarImages ( CarUnitId, ImgUrl, Remarks, SysCode)
-values (1,'/Images/CarRental/Grandia.jpg','','MAIN'),
-       (2,'/Images/CarRental/Grandia.jpg','','MAIN'),
-       (3,'/Images/CarRental/ToyotaInnova.jpg','','MAIN'),
-       (4,'/Images/CarRental/FordEverest.jpg','','MAIN'),
-       (5,'/Images/CarRental/ToyotaHilux.jpg','','MAIN'),
-       (6,'/Images/CarRental/HondaCity.jpg','','MAIN');
-
-insert into CarViewPage (CarUnitId, Viewname)
+values (1,'Grandia.jpg','','MAIN'),
+       (2,'Grandia.jpg','','MAIN'),
+       (3,'ToyotaInnova.jpg','','MAIN'),
+       (4,'FordEverest.jpg','','MAIN'),
+       (5,'ToyotaHilux.jpg','','MAIN'),
+       (6,'HondaCity.jpg','','MAIN');
+	   
+insert into CarViewPages (CarUnitId, Viewname)
 values (1,'CarDetail_van'),
 	   (2,'CarDetail_van'),
-	   (2,'CarDetail_mpv'),
-	   (2,'CarDetail_suv'),
-	   (2,'CarDetail_pickup'),
-	   (2,'CarDetail_sedan');
+	   (3,'CarDetail_mpv'),
+	   (4,'CarDetail_suv'),
+	   (5,'CarDetail_pickup'),
+	   (6,'CarDetail_sedan');
 
+insert into CarRates (Daily,Weekly,Monthly,KmFree,KmRate,CarUnitId)
+values (3500,3000,2250,120,5,1),
+	   (3500,3000,2250,120,5,2),
+	   (3500,3500,3000,100,5,3),
+	   (3500,2500,2000,100,5,4),
+	   (3500,3000,2500,100,5,5),
+	   (2500,2000,1500,100,5,6);
